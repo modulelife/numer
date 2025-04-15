b75656b8dbb99747b395aebbec8bb781fe2dc45e
pyridineC <108844893+modulelife@users.noreply.github.com>
2025-04-15 12:13:01 +0800

    25w16b

--- numer/include/numer_qmkit.h
+++ numer/include/numer_qmkit.h
@@ -35,35 +35,32 @@ namespace numer{
 			unsigned max_n_;
 			unsigned resol_;
 			std::vector<double> fcoef_;
-			mat<Complex> alphas_;
 
 		public:
 			HusimiQCalculator(unsigned Max_energy_level_, unsigned Resolution_)
-				:max_n_(Max_energy_level_ + 1), resol_(Resolution_), fcoef_(max_n_), alphas_(resol_, resol_)
+				:max_n_(Max_energy_level_ + 1), resol_(Resolution_), fcoef_(max_n_)
 			{
 				for (unsigned i = 0; i < max_n_; ++i) {
 					fcoef_[i] = 1.0 / sqrt(factorial(i));
 				}
-
-				const auto cplx_gen = [](double re, double im) { return Complex{ re, im }; };
-				const double maxamp = sqrt(max_n_ + 0.5) + sqrt(max_n_) / 2.0;
-				const RangeSpec spec{ -maxamp , maxamp , resol_ - 1 };
-				BinaryFuncSampler cplx_sampler(cplx_gen, spec, spec);
-				alphas_.refill_par(cplx_sampler);
 			}
 
 			template<class Array>
 			mat<double> calculate(Array E_rep_ket_, size_t N_) {
 				mat<Complex> a_ket_(resol_, resol_, 0.0);
+				const auto cplx_gen = [](double re, double im) { return Complex{ re, im }; };
+				const double maxamp = sqrt(max_n_ + 0.5) + sqrt(max_n_) / 2.0;
+				const RangeSpec spec{ -maxamp , maxamp , resol_ - 1 };
+				BinaryFuncSampler cplx_sampler(cplx_gen, spec, spec);
 
 				using args__ = struct { size_t i__; size_t j__; };
 				mat<args__> arg_list = mat<args__>::creat(resol_, resol_, [](size_t i, size_t j) {return args__{ i, j }; });
 
 				const auto calcu_proj = [&](const args__& arg) -> void {
-					const Complex a = alphas_[arg.i__][arg.j__].conj();
+					const Complex a = cplx_sampler(arg.i__, arg.j__).conj();
 					Complex an = 1.0, proj = 0.0;
 					double nfactor = exp(-a.sqrdAmp() / 2.0);
-					for (unsigned k = 0; k < max_n_ || k < N_; ++k) {
+					for (unsigned k = 0; k < max_n_ && k < N_; ++k) {
 						proj += an * E_rep_ket_[k] * fcoef_[k];
 						an *= a;
 					}
